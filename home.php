e64d8c250e7f33e904e584e1165aea8dae3fbf4d
Abang Muhammad Sayudi Hamzah <101570559+SayudiHamzah02@users.noreply.github.com>
2022-04-14 10:25:52 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="https://rekreartive.com/wp-content/uploads/2018/11/Logo-Untan-Universitas-Tanjungpura-PNG.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">Rekayasa Sistem Komputer</font> <br>
+<font Size="6">Universitas Tanjungpura</font>
+</center>
